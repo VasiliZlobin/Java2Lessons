f4efb30cb94e04ce6dde5e4da39625fd91f5bb7c
Vasili Zlobin <vaszlbn@gmail.com>
2022-04-11 19:52:37 +0300

    add solution lesson1

---
+++ src/com/vasili_zlobin/lesson1/CanRunAndJump.java
@@ -0,0 +1,7 @@
+package com.vasili_zlobin.lesson1;
+
+public interface CanRunAndJump {
+    int run();
+    double jump();
+    String getInfo();
+}
---
+++ src/com/vasili_zlobin/lesson1/CheckSuccess.java
@@ -0,0 +1,5 @@
+package com.vasili_zlobin.lesson1;
+
+public interface CheckSuccess {
+    boolean isSuccess(CanRunAndJump member);
+}
